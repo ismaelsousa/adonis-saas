4b30d2b86d6354dc8aa1feaecd63a94ca9f5dfaf
ismael moreira de sousa <ismaelsousa1998@outlook.com>
2020-03-17 15:35:23 -0300

    chore: add rotas de convites para usuarios entrarem no time

---
+++ app/Middleware/Team.js
@@ -0,0 +1,22 @@
+/* eslint-disable class-methods-use-this */
+
+class Team {
+  async handle({ request, response, auth }, next) {
+    const slug = request.header('TEAM');
+    let team = null;
+    if (slug) {
+      team = await auth.user.teams().where('slug', slug).first();
+    }
+
+    if (!team) {
+      return response.status(404).send();
+    }
+
+    auth.user.currentTeam = team.id;
+    request.team = team;
+
+    await next();
+  }
+}
+
+module.exports = Team;
