19e167d7800f65a808022d91195f5dd46980dcfb
ismael moreira de sousa <ismaelsousa1998@outlook.com>
2020-03-19 20:51:56 -0300

    chore: add toda parte de acl para o user

--- database/seeds/DatabaseSeeder.js
+++ database/seeds/DatabaseSeeder.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 
 /*
 |--------------------------------------------------------------------------
@@ -11,7 +12,8 @@
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const User = use('App/Models/User');
-
+const Role = use('Adonis/Acl/Role');
+const Permission = use('Adonis/Acl/Permission');
 class DatabaseSeeder {
   async run() {
     const user = await User.create({
@@ -20,10 +22,42 @@ class DatabaseSeeder {
       password: '123455',
     });
 
-    await user.teams().create({
+    const createInvite = await Permission.create({
+      slug: 'invites_create',
+      name: 'Convidar membros',
+    });
+
+    const createProject = await Permission.create({
+      slug: 'projects_create',
+      name: 'Criar projetos',
+    });
+
+
+    const admin = await Role.create({
+      slug: 'administrator',
+      name: 'Administrador',
+    });
+
+    const moderator = await Role.create({
+      slug: 'moderator',
+      name: 'Moderador',
+    });
+
+    await Role.create({
+      slug: 'visitor',
+      name: 'Visitante',
+    });
+
+    await admin.permissions().attach([createInvite.id, createProject.id]);
+    await moderator.permissions().attach([createProject.id]);
+
+    const team = await user.teams().create({
       name: 'taki',
       user_id: user.id,
     });
+
+    const teamJoin = await user.teamJoins().where('team_id', team.id).first();
+    await teamJoin.roles().attach([admin.id]);
   }
 }
 
